1f550f2ccf3c9cb37c2f6a83a804b1405d27f271
fdaniia <f.daniyusha@ggmail.com>
2025-10-03 10:13:08 +0500

    Добавьте файлы проекта.

---
+++ Lab2_rab_FazyilovaD.I_bpi-23-02/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab2_rab_FazyilovaD.I_bpi_23_02
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
